d21a379c3931844c188e277c2fd958b876168d5c
unknown <monikadesign54@gmail.com>
2019-12-26 18:25:02 +0530

    admin-template-demo

---
+++ admin-template/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,89 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+import * as Chartist from 'chartist';
+import { ChartType, ChartEvent } from 'ng-chartist';
+
+
+
+declare var require: any;
+
+const data: any = require('./data.json');
+
+export interface Chart {
+	type: ChartType;
+	data: Chartist.IChartistData;
+	options?: any;
+	responsiveOptions?: any;
+	events?: ChartEvent;
+}
+
+@Component({
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements AfterViewInit {
+	ngAfterViewInit() {}
+
+	// Barchart
+	barChart1: Chart = {
+		type: 'Bar',
+		data: data['Bar'],
+		options: {
+			seriesBarDistance: 15,
+			high:10,
+
+			axisX: {
+				showGrid: false,
+				offset: 20
+			},
+			axisY: {
+				showGrid: true,
+				offset: 40
+			},
+			height: 360
+		},
+		events: {
+			draw: (data) => {
+			  if (data.type === 'bar') {
+				data.element.animate({
+				  y2:{
+					dur: '0.5s',
+					from: data.y1,
+					to: data.y2,
+					easing: 'easeOutQuad'
+				  }
+				});
+			  }
+			}
+		  },
+
+		responsiveOptions: [
+			[
+				'screen and (min-width: 640px)',
+				{
+					axisX: {
+						labelInterpolationFnc: function(
+							value: number,
+							index: number
+						): string {
+							return index % 1 === 0 ? `${value}` : null;
+						}
+					}
+				}
+			]
+		]
+	};
+
+	// This is for the donute chart
+	donuteChart1: Chart = {
+		type: 'Pie',
+		data: data['Pie'],
+		options: {
+			donut: true,
+			height: 260,
+			showLabel: false,
+			donutWidth: 20
+		}
+	};
+}
